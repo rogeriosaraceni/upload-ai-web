ee1c7ad114784240b18397fddb9c3d48539fbd68
rogeriosaraceni <rsaraceni@gmail.com>
2023-09-18 21:40:27 -0300

    Finalizando staus do botão

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -8,10 +8,20 @@ import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 import { api } from "@/lib/axios";
 
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success';
+
+const statusMessages = {
+    converting: 'Convertendo...',
+    generating: 'Transcrevendo...',
+    uploading: 'Carregando...',
+    success: 'Sucesso!',
+}
+
 export function VideoInputForm() {
     //1 parametro nome do estado
     //2 parametro recebe uma função p/ alterar o valor do estado
     const [videoFile, setVideoFile] = useState<File | null>(null)
+    const [status, setStatus] = useState<Status>('waiting')
 
     const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
@@ -77,6 +87,9 @@ export function VideoInputForm() {
             return
         }
 
+        //antes de converter trocar o status
+        setStatus('converting')
+
         //converter o vídeo em áudio
         const audioFile = await converterVideoToAudio(videoFile)
 
@@ -86,17 +99,23 @@ export function VideoInputForm() {
 
         data.append('file', audioFile)
 
+        setStatus('uploading')
+
         const response = await api.post('/videos', data)
 
         //console.log(response.data);
 
         const videoId = response.data.video.id
 
+        setStatus('generating')
+
         await api.post(`/videos/${videoId}/transcription`, {
             prompt,
         })
 
         console.log('finalizou');
+
+        setStatus('success')
     }
 
     //Hook useMemo: recebe 1 parametro uma function e como 2 parametro um array de dependências
@@ -132,15 +151,25 @@ export function VideoInputForm() {
                 <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
                 <Textarea
                     ref={promptInputRef}
+                    disabled={status !== 'waiting'}
                     id="transcription_prompt"
                     className="h-20 leading-relaxed resize-none"
                     placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
                 />
             </div>
 
-            <Button type="submit" className="w-full">
-                Carregar vídeo
-                <Upload className="w-4 h-4 ml-2" />
+            <Button
+                data-success={status === 'success'}
+                disabled={status !== 'waiting'}
+                type="submit"
+                className="w-full data-[success=true]:bg-emerald-400"
+            >
+                {status === 'waiting' ? (
+                    <>
+                        Carregar vídeo
+                        <Upload className="w-4 h-4 ml-2" />
+                    </>
+                ) : statusMessages[status]}
             </Button>
         </form>
     )
